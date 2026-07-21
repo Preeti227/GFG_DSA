31a65aad3e87bc22b4e03bf573c169f396599854
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-07-21 11:41:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Square Submatrices with All Ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSquares(int[][] mat) {
+        int m=mat.length, n=mat[0].length;
+        int[][]dp=new int[m][n];
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    if(i==0||j==0) dp[i][j]=1;
+                    else{
+                        dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                    }
+                    count+=dp[i][j];
+                }
+            }
+        }
+        return count;
+        
+        
+    }
+}
