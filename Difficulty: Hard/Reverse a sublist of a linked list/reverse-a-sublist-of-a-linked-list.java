3ba3f933852bb2cde1d43c7034408c580831b19a
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-09-05 21:04:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
+++ Difficulty: Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
@@ -1,31 +1,40 @@
-
 class Solution {
-    Node reverseBetween(int left, int right, Node head) {
-        ArrayList<Node> list = new ArrayList<>();
-        Node temp = head;
+    Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while(curr != null) {
+            Node fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        return prev;
+    }
 
-        while(temp != null) {
-            list.add(temp);
+    Node reverseBetween(int a, int b, Node head) {
+        Node dummy = new Node(-1);
+        dummy.next = head;
+        Node temp = dummy;
+        for(int i = 1; i < a; i++) {// Move to node just before position a
             temp = temp.next;
         }
+        Node tail1 = temp;
+        Node head2 = temp.next;
 
-        int i = left - 1;
-        int j = right - 1;
-        while(i < j) {
-            Node t1 = list.get(i);
-            Node t2 = list.get(j);
-            list.set(i, t2);
-            list.set(j, t1);
-            i++;
-            j--;
+        temp = head2;
+        for(int i = a; i < b; i++) {// Move to position b
+            temp = temp.next;
         }
+        Node tail2 = temp;
+        Node head3 = tail2.next;
 
-        for(i = 0; i < list.size(); i++) {
-            if(i == list.size() - 1)
-                list.get(i).next = null;
-            else
-                list.get(i).next = list.get(i + 1);
-        }
-        return list.get(0);
+        tail2.next = null;
+
+        Node newHead2 = reverse(head2);
+
+        tail1.next = newHead2;
+        head2.next = head3;
+
+        return dummy.next;
     }
 }
